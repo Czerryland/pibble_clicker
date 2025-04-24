f600ab635d649064c26bf597e2f335673832721d
Grzegorz <grzes.labedzki@gmail.com>
2025-04-25 00:19:28 +0200

    pibble!

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 const pibbclick = document.getElementById("pibbclick");
-const pointdisplay = document.getElementById("point-display");
-const displayUpgrades = document.getElementById("upgrades");
-const upgradeList = document.getElementById("upgrade-list");
-const returnbtn = document.getElementById("return");
+const pointdisplay = document.getElementById("point-display");;
 const animbox = document.getElementById("animbox");
+const washingtonUpgrade = document.getElementById("washington-buy");
+const emopibbUpgrade = document.getElementById("emopibble-buy");
+
+
 
 let score = 0;
+let pibblePower = 1;
+let pibbMultiplayer = 1;
+
+let emopibbs = 0;
+
+function updateScore(increment){
+    score += increment;
+    pointdisplay.textContent = "Pibbs: "+score;
+}
+
+function idlePibbs(){
+    updateScore(5 * emopibbs)
+}
 
 pibbclick.addEventListener('click',()=>{
-    score += 1;
-    pointdisplay.textContent = "Pibbs: "+score;
+    updateScore(pibblePower);
     animbox.classList.remove('pibbClickAnim');
-
-
-
     void animbox.offsetWidth; 
-
-
-
     animbox.classList.add('pibbClickAnim');
 });
 
-displayUpgrades.addEventListener('click',()=>{
-    upgradeList.style.display = 'inline'
+washingtonUpgrade.addEventListener('click',()=>{
+    if (score >=10){
+        updateScore(-10);
+        pibblePower += 1;
+    }
 });
 
-returnbtn.addEventListener('click',()=>{
-    upgradeList.style.display = 'none'
+emopibbUpgrade.addEventListener('click',()=>{
+    if (score >=15){
+        updateScore(-15);
+        emopibbs += 1;
+    }
 });
+
+setInterval(idlePibbs, 5000)
